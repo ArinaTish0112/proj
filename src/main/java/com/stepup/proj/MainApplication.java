aa72c13e99424c72218391760a4ec3165fde0638
Alexander Fisunov <git.user.1989@gmail.com>
2025-08-30 12:24:38 +0300

    Доработка логики проекта

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -4,6 +4,5 @@ public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("1111111");
-        System.out.println("1111111");
     }
 }
